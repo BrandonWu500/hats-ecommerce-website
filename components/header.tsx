237946e2b6e34e4fc71b457afe660bca3c5effd9
BrandonWu500 <brwu17@gmail.com>
2023-09-23 19:00:22 -0400

    feat: add logo to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,10 @@
+import Logo from '@/components/logo';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="h-[80px] bg-slate-600">
+      <Logo />
+    </div>
+  );
 };
 export default Header;
