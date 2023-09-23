998590a09e1d3e10a0a1bedd173923a8bcfdbf7f
BrandonWu500 <brwu17@gmail.com>
2023-09-23 18:59:49 -0400

    feat: create logo.tsx

---
+++ components/logo.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '/public/logo.svg';
+
+const Logo = () => {
+  return (
+    <Link href={'/'} className="flex items-center gap-2 p-4">
+      <Image src={logo} alt="Logo" />
+      <p className="font-heading text-3xl font-semibold text-orange-200">
+        Hats
+      </p>
+    </Link>
+  );
+};
+export default Logo;
