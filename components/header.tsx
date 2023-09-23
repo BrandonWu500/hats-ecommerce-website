1380319100232b67834aa14abebce9c0ec677806
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-09-23 19:56:14 -0400

    feat: create desktop header (#10)

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,24 @@
 import Image from 'next/image';
 
+import Container from '@/components/container';
 import Logo from '@/components/logo';
 
 import shoppingBag from '/public/shopping-bag.svg';
 
 const Header = () => {
   return (
-    <div className="flex h-[80px] items-center justify-between bg-slate-600 px-4 pl-0">
-      <Logo />
+    <div className="flex h-[80px] justify-center bg-slate-600 px-4 pl-0 xl:h-[100px] xl:pr-0">
+      <Container>
+        <Logo />
 
-      {/* CART BUTTON */}
-      <button className="flex items-center justify-center gap-4 rounded-full bg-orange-200 px-6 py-2">
-        <div className="relative h-6 w-6">
-          <Image src={shoppingBag} alt="shopping bag" fill />
-        </div>
-        <p className="font-body text-2xl font-medium text-slate-600">0</p>
-      </button>
+        {/* CART BUTTON */}
+        <button className="flex items-center justify-center gap-3 rounded-full bg-orange-200 px-4 py-2 xl:gap-4">
+          <div className="relative h-6 w-6">
+            <Image src={shoppingBag} alt="shopping bag" fill />
+          </div>
+          <p className="font-body text-2xl font-medium text-slate-600">0</p>
+        </button>
+      </Container>
     </div>
   );
 };
