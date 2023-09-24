6533b349701f7c159623b255d0cd547dc441a301
BrandonWu500 <brwu17@gmail.com>
2023-09-24 13:11:31 -0400

    feat: add billboard content

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -1,4 +1,10 @@
 const Billboard = () => {
-  return <div>Billboard</div>;
+  return (
+    <div className="w-full rounded-[10px] bg-orange-200 px-8 py-12">
+      <h1 className="text-center font-heading text-[40px] font-semibold uppercase text-slate-600">
+        Find the Perfect Hat for You!
+      </h1>
+    </div>
+  );
 };
 export default Billboard;
