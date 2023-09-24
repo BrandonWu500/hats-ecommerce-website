f051e3218565a6d50949bdccadb0751219e3ccd5
BrandonWu500 <brwu17@gmail.com>
2023-09-24 13:26:04 -0400

    feat: create desktop billboard

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -1,7 +1,7 @@
 const Billboard = () => {
   return (
-    <div className="w-full rounded-[10px] bg-orange-200 px-8 py-12">
-      <h1 className="text-center font-heading text-[40px] font-semibold uppercase text-slate-600">
+    <div className="w-full rounded-[10px] bg-orange-200 px-8 py-12 xl:px-24 xl:py-16">
+      <h1 className="text-center font-heading text-[40px] font-semibold uppercase leading-tight text-slate-600 xl:text-[72px]">
         Find the Perfect Hat for You!
       </h1>
     </div>
