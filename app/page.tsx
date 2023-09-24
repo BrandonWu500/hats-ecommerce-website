f7e3e263ba75ad17d1abf9066349e492c5926b24
BrandonWu500 <brwu17@gmail.com>
2023-09-24 13:30:41 -0400

    feat: add products to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Billboard from '@/components/billboard';
 import Container from '@/components/container';
+import Products from '@/components/products';
 
 const HomePage = () => {
   return (
     <Container>
       <Billboard />
+      <Products />
     </Container>
   );
 };
