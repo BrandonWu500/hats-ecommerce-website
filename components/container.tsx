843756f34579e1d103e4e25679a3015d66e3405a
BrandonWu500 <brwu17@gmail.com>
2023-09-24 13:21:27 -0400

    feat: add mx-auto to container

--- components/container.tsx
+++ components/container.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 const Container = ({ children }: Props) => {
   return (
-    <div className="flex w-full max-w-screen-xl items-center justify-between px-4 xl:px-[140px]">
+    <div className="mx-auto flex w-full max-w-screen-xl items-center justify-between px-4 xl:px-[140px]">
       {children}
     </div>
   );
