4518d70e0224da2be049ce72587915dc5c2c6c16
BrandonWu500 <brwu17@gmail.com>
2023-09-24 14:56:55 -0400

    feat: add price component to product card

--- components/products/product-card.tsx
+++ components/products/product-card.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import Price from '@/components/price';
+
 type Props = {
   imgUrl: string;
   title: string;
@@ -21,7 +23,7 @@ const ProductCard = ({ imgUrl, title, price, handle }: Props) => {
         </div>
         <div className="flex flex-col gap-2 text-center font-heading">
           <h3 className="text-[32px] font-semibold">{title}</h3>
-          <p className="text-2xl font-medium">{price}</p>
+          <Price amount={price} className="text-2xl font-medium" />
         </div>
       </Link>
     </li>
