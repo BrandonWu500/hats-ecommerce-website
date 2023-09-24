ead799a075608f6dbc3a5f159e7d5f90ec88393f
BrandonWu500 <brwu17@gmail.com>
2023-09-24 14:57:49 -0400

    feat: add products content
    static data for now

--- components/products/index.tsx
+++ components/products/index.tsx
@@ -1,4 +1,32 @@
+import ProductCard from '@/components/products/product-card';
+
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div>
+      <h2 className="mb-12 text-center font-heading text-[32px] font-semibold">
+        Products
+      </h2>
+      <ul className="flex flex-col gap-16">
+        <ProductCard
+          imgUrl={'/hat-1.jpeg'}
+          handle="test"
+          price={49}
+          title="Palm Tree"
+        />
+        <ProductCard
+          imgUrl={'/hat-1.jpeg'}
+          handle="test"
+          price={49}
+          title="Palm Tree"
+        />
+        <ProductCard
+          imgUrl={'/hat-1.jpeg'}
+          handle="test"
+          price={49}
+          title="Palm Tree"
+        />
+      </ul>
+    </div>
+  );
 };
 export default Products;
