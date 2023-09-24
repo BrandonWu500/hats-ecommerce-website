2ff36e3fed30391a9286bcd4177d18e1105f63ca
BrandonWu500 <brwu17@gmail.com>
2023-09-24 13:34:27 -0400

    feat: add className override prop to container component

--- components/container.tsx
+++ components/container.tsx
@@ -1,9 +1,17 @@
+import { twMerge } from 'tailwind-merge';
+
 type Props = {
   children: React.ReactNode;
+  className?: string;
 };
-const Container = ({ children }: Props) => {
+const Container = ({ children, className }: Props) => {
   return (
-    <div className="mx-auto flex w-full max-w-screen-xl items-center justify-between px-4 xl:px-[140px]">
+    <div
+      className={twMerge(
+        'mx-auto flex w-full max-w-screen-xl items-center justify-between px-4 xl:px-[140px]',
+        className
+      )}
+    >
       {children}
     </div>
   );
