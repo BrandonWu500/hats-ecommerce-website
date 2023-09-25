0579039046035c1feca1d72f80626fad7c1711b9
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-09-25 14:36:43 -0400

    feat: create desktop products section (#15)
    and add shopify revalidate functionality
    
    Reference for setting up shopify revalidation:
    https://github.com/vercel/commerce and
    https://vercel.com/docs/integrations/shopify

--- components/products/index.tsx
+++ components/products/index.tsx
@@ -5,11 +5,11 @@ const Products = async () => {
   const products = await getProducts({});
 
   return (
-    <div>
-      <h2 className="mb-12 text-center font-heading text-[32px] font-semibold">
+    <div className="mb-16 xl:mt-8">
+      <h2 className="mb-12 text-center font-heading text-[32px] font-semibold xl:mb-16 xl:text-left xl:text-5xl">
         Products
       </h2>
-      <ul className="flex flex-col gap-16">
+      <ul className="grid grid-cols-1 gap-16 xl:grid-cols-3 xl:gap-x-8 xl:gap-y-[72px]">
         {products.map((product) => (
           <ProductCard
             key={product.handle}
