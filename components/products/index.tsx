8e36399f83e2a23d092e753a854d154ae7e57cc3
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-09-25 10:30:06 -0400

    feat: fetch products from shopify (#14)
    Reference for setting up Shopify: https://github.com/vercel/commerce and
    https://vercel.com/docs/integrations/shopify

--- components/products/index.tsx
+++ components/products/index.tsx
@@ -1,30 +1,24 @@
 import ProductCard from '@/components/products/product-card';
+import { getProducts } from '@/lib/shopify';
 
-const Products = () => {
+const Products = async () => {
+  const products = await getProducts({});
+
   return (
     <div>
       <h2 className="mb-12 text-center font-heading text-[32px] font-semibold">
         Products
       </h2>
       <ul className="flex flex-col gap-16">
-        <ProductCard
-          imgUrl={'/hat-1.jpeg'}
-          handle="test"
-          price={49}
-          title="Palm Tree"
-        />
-        <ProductCard
-          imgUrl={'/hat-1.jpeg'}
-          handle="test"
-          price={49}
-          title="Palm Tree"
-        />
-        <ProductCard
-          imgUrl={'/hat-1.jpeg'}
-          handle="test"
-          price={49}
-          title="Palm Tree"
-        />
+        {products.map((product) => (
+          <ProductCard
+            key={product.handle}
+            imgUrl={product.featuredImage.url}
+            handle={product.handle}
+            price={product.priceRange.maxVariantPrice.amount}
+            title={product.title}
+          />
+        ))}
       </ul>
     </div>
   );
