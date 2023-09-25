b7d5f7cab4b3731e4af6bf6d5610fc6a8a582ce6
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-09-25 17:42:02 -0400

    feat: update logo and favicon (#16)
    and add website title

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,13 +1,10 @@
-import Image from 'next/image';
 import Link from 'next/link';
-import logo from '/public/logo.svg';
 
 const Logo = () => {
   return (
     <Link href={'/'} className="flex items-center gap-2 py-4  xl:gap-4">
-      <Image src={logo} alt="Logo" />
       <p className="font-heading text-3xl font-semibold text-orange-200">
-        Hats
+        BW Hats
       </p>
     </Link>
   );
