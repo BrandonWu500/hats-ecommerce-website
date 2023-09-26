c668bb9c7336f42e1d7c3fb897e15330fd047abc
BrandonWu500 <brwu17@gmail.com>
2023-09-26 16:01:05 -0400

    feat: add alternate images display

--- components/products/product-gallery.tsx
+++ components/products/product-gallery.tsx
@@ -9,8 +9,8 @@ type Props = {
 const ProductGallery = ({ images }: Props) => {
   console.log(images);
   return (
-    <div>
-      <div className="relative h-[291px] w-[338px]">
+    <div className="flex w-full flex-col gap-8 pb-8">
+      <div className="relative mx-auto h-[291px] w-[338px]">
         <Image
           src={images[0].src}
           alt={images[0].altText}
@@ -18,6 +18,23 @@ const ProductGallery = ({ images }: Props) => {
           className="object-cover"
         />
       </div>
+      {images.length > 1 && (
+        <div className="flex w-full gap-8 overflow-x-auto sm:justify-center">
+          {images.map((image) => (
+            <div
+              key={image.src}
+              className="relative h-[119px] w-[124px] flex-shrink-0"
+            >
+              <Image
+                src={image.src}
+                alt={image.altText}
+                fill
+                className="object-cover"
+              />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
