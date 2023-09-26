69f5a30a4c2c681ca129a323e1d5de4cb67427d5
BrandonWu500 <brwu17@gmail.com>
2023-09-26 14:23:11 -0400

    feat: add main image to product gallery

--- components/products/product-gallery.tsx
+++ components/products/product-gallery.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 type Props = {
   images: {
     src: string;
@@ -6,6 +8,17 @@ type Props = {
 };
 const ProductGallery = ({ images }: Props) => {
   console.log(images);
-  return <div>ProductGallery</div>;
+  return (
+    <div>
+      <div className="relative h-[291px] w-[338px]">
+        <Image
+          src={images[0].src}
+          alt={images[0].altText}
+          fill
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
 };
 export default ProductGallery;
