3dc701d6f93730f9b5631e69d5057e54c5a142c2
BrandonWu500 <brwu17@gmail.com>
2023-09-26 09:48:20 -0400

    feat: add product gallery to product page

--- app/product/[handle]/page.tsx
+++ app/product/[handle]/page.tsx
@@ -1,10 +1,27 @@
+import { notFound } from 'next/navigation';
+
+import Container from '@/components/container';
+import ProductGallery from '@/components/products/product-gallery';
 import { getProduct } from '@/lib/shopify';
+import { Image } from '@/lib/shopify/types';
 
 type Props = {
   params: { handle: string };
 };
 const ProductPage = async ({ params }: Props) => {
   const product = await getProduct(params.handle);
-  return <div>{product?.title}</div>;
+
+  if (!product) return notFound();
+
+  return (
+    <Container>
+      <ProductGallery
+        images={product.images.map((image: Image) => ({
+          src: image.url,
+          altText: image.altText,
+        }))}
+      />
+    </Container>
+  );
 };
 export default ProductPage;
