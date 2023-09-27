aeb91e7010c6a73fd3e700cfbde9fb99ed4c5e46
BrandonWu500 <brwu17@gmail.com>
2023-09-27 10:22:27 -0400

    feat: create desktop product info section

--- components/products/product-info.tsx
+++ components/products/product-info.tsx
@@ -6,13 +6,13 @@ type Props = {
 };
 const ProductInfo = ({ product }: Props) => {
   return (
-    <div className="my-4 flex flex-col gap-6">
-      <h1 className="text-center font-heading text-5xl font-semibold">
+    <div className="my-4 flex flex-col gap-6 xl:mt-0">
+      <h1 className="text-center font-heading text-5xl font-semibold xl:text-start xl:text-[72px]">
         {product.title}
       </h1>
       {product.descriptionHtml ? (
         <Prose
-          className="mx-auto max-w-[372px]"
+          className="mx-auto max-w-[372px] xl:max-w-none"
           html={product.descriptionHtml}
         />
       ) : null}
