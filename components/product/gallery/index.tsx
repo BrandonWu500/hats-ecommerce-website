5f5efa3e13a9c1728d5cab932bbdc0f238093c5d
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-09-27 17:39:01 -0400

    feat: update gallery image tabs (#25)

---
+++ components/product/gallery/index.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { Tab } from '@headlessui/react';
+import Image from 'next/image';
+
+import ProductGalleryTab from '@/components/product/gallery/tab';
+
+type Props = {
+  images: {
+    src: string;
+    altText: string;
+  }[];
+};
+const ProductGallery = ({ images }: Props) => {
+  return (
+    <Tab.Group as="div" className="flex w-full flex-col gap-8 pb-8">
+      <Tab.Panels>
+        {images.map((image) => (
+          <Tab.Panel key={image.src}>
+            <div className="relative mx-auto h-[291px] w-[338px] xl:m-0 xl:h-[605px] xl:w-[571px]">
+              <Image
+                src={image.src}
+                alt={image.altText}
+                fill
+                className="rounded-[10px] object-cover"
+                sizes="(min-width: 1280px) 571px, 338px"
+              />
+            </div>
+          </Tab.Panel>
+        ))}
+      </Tab.Panels>
+      {images.length > 1 && (
+        <Tab.List className="mx-auto flex w-full max-w-[372px] gap-8 overflow-x-auto xl:ml-0 xl:max-w-[571px]">
+          {images.map((image) => (
+            <ProductGalleryTab key={image.src} image={image} />
+          ))}
+        </Tab.List>
+      )}
+    </Tab.Group>
+  );
+};
+export default ProductGallery;
