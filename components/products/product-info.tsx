ea9378bc945349ff6e8de7e90fad8feec3340de2
BrandonWu500 <brwu17@gmail.com>
2023-09-27 09:53:52 -0400

    feat: add product info content

--- components/products/product-info.tsx
+++ components/products/product-info.tsx
@@ -1,10 +1,22 @@
+import Prose from '@/components/prose';
 import { Product } from '@/lib/shopify/types';
 
 type Props = {
   product: Product;
 };
 const ProductInfo = ({ product }: Props) => {
-  console.log(product);
-  return <div>ProductInfo</div>;
+  return (
+    <div className="my-4 flex flex-col gap-6">
+      <h1 className="text-center font-heading text-5xl font-semibold">
+        {product.title}
+      </h1>
+      {product.descriptionHtml ? (
+        <Prose
+          className="mx-auto max-w-[372px]"
+          html={product.descriptionHtml}
+        />
+      ) : null}
+    </div>
+  );
 };
 export default ProductInfo;
