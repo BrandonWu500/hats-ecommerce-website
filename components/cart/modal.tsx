bd60484aead164a038a314cb65f8cae0ed340420
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-09-28 16:09:12 -0400

    feat: create mini cart item display (#31)

--- components/cart/modal.tsx
+++ components/cart/modal.tsx
@@ -2,8 +2,11 @@
 
 import { Dialog } from '@headlessui/react';
 import { ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/solid';
+import Image from 'next/image';
 import { useState } from 'react';
 
+import Price from '@/components/price';
+import Quantity from '@/components/quantity';
 import { Cart } from '@/lib/shopify/types';
 
 type Props = {
@@ -28,7 +31,7 @@ const CartModal = ({ cart }: Props) => {
       </button>
 
       <Dialog open={isOpen} onClose={closeCart} className="relative z-50">
-        <Dialog.Panel className="fixed bottom-0 right-0 top-0 flex h-full w-full flex-col border-l border-slate-200 bg-slate-100 p-6 md:w-[390px]">
+        <Dialog.Panel className="fixed bottom-0 right-0 top-0 flex h-full w-full flex-col gap-12 border-l border-slate-200 bg-slate-100 p-6 md:w-[390px]">
           <div className="flex items-center justify-between">
             <p className="font-heading text-[28px] font-medium">Shopping Bag</p>
 
@@ -37,12 +40,78 @@ const CartModal = ({ cart }: Props) => {
             </button>
           </div>
 
-          <div className="mt-20 flex w-full flex-col items-center justify-center overflow-hidden">
-            <ShoppingBagIcon className="h-16" />
-            <p className="mt-6 text-center font-heading text-3xl font-semibold">
-              Your bag is empty.
-            </p>
-          </div>
+          {!cart || cart.lines.length === 0 ? (
+            <div className="mt-20 flex w-full flex-col items-center justify-center overflow-hidden">
+              <ShoppingBagIcon className="h-16" />
+              <p className="mt-6 text-center font-heading text-3xl font-semibold">
+                Your bag is empty.
+              </p>
+            </div>
+          ) : (
+            <ul className="flex flex-col gap-6 overflow-y-auto">
+              {cart.lines.map((item) => (
+                <li key={item.id} className="flex gap-4">
+                  <div className="relative h-[255px] w-[162px]">
+                    <Image
+                      className="rounded-[10px] object-cover"
+                      fill
+                      sizes="162px"
+                      alt={
+                        item.merchandise.product.featuredImage.altText ||
+                        item.merchandise.product.title
+                      }
+                      src={item.merchandise.product.featuredImage.url}
+                    />
+                  </div>
+                  <div className="flex flex-col gap-8">
+                    <div className="flex flex-col gap-2">
+                      <p className="font-heading text-2xl font-semibold">
+                        {item.merchandise.product.title}
+                      </p>
+                      <Price
+                        className="font-body text-xl"
+                        amount={item.cost.totalAmount.amount}
+                        currencyCode={item.cost.totalAmount.currencyCode}
+                      />
+                    </div>
+                    <div className="-translate-x-1">
+                      <Quantity quantity={item.quantity} />
+                    </div>
+                    <p className="font-heading text-xl underline">Remove</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+          {cart && (
+            <>
+              <div className="mb-3 flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                  <p className="font-heading text-2xl font-semibold">
+                    Subtotal
+                  </p>
+                  <span className="font-body text-lg">
+                    {cart.totalQuantity === 1
+                      ? `(${cart.totalQuantity} item)`
+                      : `(${cart.totalQuantity} items)`}
+                  </span>
+                </div>
+                <Price
+                  className="text-right text-2xl font-bold"
+                  amount={cart.cost.totalAmount.amount}
+                  currencyCode={cart.cost.totalAmount.currencyCode}
+                />
+              </div>
+              <div className="flex flex-col gap-8">
+                <button className="rounded-full bg-orange-200 px-8 py-4 text-center font-heading text-2xl font-medium">
+                  Proceed To Checkout
+                </button>
+                <button className="rounded-full border-2 border-slate-700 bg-transparent px-8 py-4 text-center font-heading text-2xl font-medium">
+                  Continue Shopping
+                </button>
+              </div>
+            </>
+          )}
         </Dialog.Panel>
       </Dialog>
     </>
