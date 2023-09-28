c3cb6ce43bbcdca183d65246df8dd977201408d8
BrandonWu500 <brwu17@gmail.com>
2023-09-28 11:19:25 -0400

    feat: add cart modal empty state

--- components/cart/modal.tsx
+++ components/cart/modal.tsx
@@ -1,10 +1,51 @@
+'use client';
+
 import { Cart } from '@/lib/shopify/types';
+import { Dialog } from '@headlessui/react';
+import { ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/solid';
+import { useState } from 'react';
 
 type Props = {
   cart: Cart | undefined;
 };
 const CartModal = ({ cart }: Props) => {
   console.log(cart);
-  return <div>CartModal</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const openCart = () => setIsOpen(true);
+  const closeCart = () => setIsOpen(false);
+
+  return (
+    <>
+      {/* CART BUTTON */}
+      <button
+        className="flex items-center justify-center gap-3 rounded-full bg-orange-200 px-4 py-2 text-slate-600 xl:gap-4"
+        aria-label="Open cart"
+        onClick={openCart}
+      >
+        <div className="relative h-6 w-6">
+          <ShoppingBagIcon />
+        </div>
+        <p className="font-body text-2xl font-medium">0</p>
+      </button>
+      <Dialog open={isOpen} onClose={closeCart} className="relative z-50">
+        <Dialog.Panel className="fixed bottom-0 right-0 top-0 flex h-full w-full flex-col border-l border-slate-200 bg-slate-100 p-6 md:w-[390px]">
+          <div className="flex items-center justify-between">
+            <p className="text-lg font-semibold">My Cart</p>
+
+            <button aria-label="Close cart" onClick={closeCart}>
+              <XMarkIcon className="h-9" />
+            </button>
+          </div>
+
+          <div className="mt-20 flex w-full flex-col items-center justify-center overflow-hidden">
+            <ShoppingBagIcon className="h-16" />
+            <p className="mt-6 text-center text-2xl font-bold">
+              Your bag is empty.
+            </p>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </>
+  );
 };
 export default CartModal;
