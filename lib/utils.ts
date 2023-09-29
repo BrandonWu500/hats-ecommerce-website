b3fa6156c16d7430007f3e320d1b2dfd4bfb8348
BrandonWu500 <brwu17@gmail.com>
2023-09-29 19:33:28 -0400

    feat: add range util function

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,3 +14,10 @@ export const ensureStartsWith = (stringToCheck: string, startsWith: string) =>
   stringToCheck.startsWith(startsWith)
     ? stringToCheck
     : `${startsWith}${stringToCheck}`;
+
+export const range = (
+  size: number,
+  startAt: number = 0
+): ReadonlyArray<number> => {
+  return [...Array(size).keys()].map((i) => i + startAt);
+};
