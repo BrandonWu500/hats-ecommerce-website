842bd0082c4e8fbcb5e86e1b1391f227cd619d1e
BrandonWu500 <brwu17@gmail.com>
2023-10-04 12:26:37 -0400

    feat: add min height to main page content

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         className={`${raleway.variable} ${roboto.variable} bg-slate-100 text-slate-700 xl:scrollbar xl:scrollbar-track-orange-200 xl:scrollbar-thumb-slate-700`}
       >
         <Header />
-        {children}
+        <main className="min-h-[calc(100vh-297px)]">{children}</main>
         <Footer />
       </body>
     </html>
