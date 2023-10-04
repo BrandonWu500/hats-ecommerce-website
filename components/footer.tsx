dd95561c7d56299c2a6e7259fd19f11c8a89687c
BrandonWu500 <brwu17@gmail.com>
2023-10-04 09:39:02 -0400

    feat: add footer links

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,26 @@
-import Container from '@/components/container';
+import Link from 'next/link';
 
-const Footer = () => {
+import Container from '@/components/container';
+import { getMenu } from '@/lib/shopify';
+
+const Footer = async () => {
+  const menu = await getMenu('footer');
+
   return (
-    <Container className="xl:mt-16">
-      <div className="w-full border-t border-slate-600">
-        <p className="pb-8 pt-4 text-center font-body text-lg text-slate-600">{`© 2023 BW Hats Inc. All rights reserved.`}</p>
-      </div>
-    </Container>
+    <div className="bg-slate-600 text-orange-200">
+      <Container className="flex-col xl:mt-16">
+        <ul className="flex flex-col items-center justify-center gap-6 py-6 pb-10 font-body text-lg ">
+          {menu.map((item) => (
+            <li key={item.title}>
+              <Link href={item.path}>{item.title}</Link>
+            </li>
+          ))}
+        </ul>
+        <div className="w-full border-t border-orange-200">
+          <p className="pb-8 pt-4 text-center font-body">{`© 2023 BW Hats Inc. All rights reserved.`}</p>
+        </div>
+      </Container>
+    </div>
   );
 };
 export default Footer;
