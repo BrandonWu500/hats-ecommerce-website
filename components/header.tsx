40cf89320e6dd04be42fb2440bb658471efd0bc9
BrandonWu500 <brwu17@gmail.com>
2023-10-05 10:51:20 -0400

    feat: add searchbar to header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,14 @@
 import Cart from '@/components/cart';
 import Container from '@/components/container';
 import Logo from '@/components/logo';
+import Searchbar from '@/components/searchbar';
 
 const Header = () => {
   return (
     <div className="sticky top-0 z-50 mb-8 flex h-[80px] justify-center bg-slate-600 xl:h-[100px] xl:pr-0">
       <Container>
         <Logo />
+        <Searchbar />
         <Cart />
       </Container>
     </div>
