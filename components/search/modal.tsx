7d2e908fa5d4bb4f806b5ad78233427e6d651fe7
BrandonWu500 <brwu17@gmail.com>
2023-10-05 13:46:01 -0400

    feat: update search modal styles

--- components/search/modal.tsx
+++ components/search/modal.tsx
@@ -4,7 +4,7 @@ import { Dialog, Transition } from '@headlessui/react';
 import { MagnifyingGlassIcon, XMarkIcon } from '@heroicons/react/24/solid';
 import { Fragment, useState } from 'react';
 
-import Searchbar from '@/components/search/bar';
+import SearchBar from '@/components/search/bar';
 
 const SearchModal = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -31,31 +31,30 @@ const SearchModal = () => {
             leaveFrom="opacity-100 backdrop-blur-[.5px]"
             leaveTo="opacity-0 backdrop-blur-none"
           >
-            <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+            {/* OVERLAY */}
+            <div className="fixed inset-0 bg-slate-900/30" aria-hidden="true" />
           </Transition.Child>
           <Transition.Child
             as={Fragment}
             enter="transition-all ease-in-out duration-300"
-            enterFrom="translate-x-[-100%]"
+            enterFrom="translate-x-full"
             enterTo="translate-x-0"
             leave="transition-all ease-in-out duration-200"
             leaveFrom="translate-x-0"
-            leaveTo="translate-x-[-100%]"
+            leaveTo="translate-x-full"
           >
-            <Dialog.Panel className="fixed bottom-0 left-0 right-0 top-0 flex h-full w-full flex-col bg-white pb-6 dark:bg-black">
-              <div className="p-4">
-                <button
-                  className="mb-4 flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white"
-                  onClick={onClose}
-                  aria-label="Hide search field"
-                >
-                  <XMarkIcon className="h-6" />
-                </button>
+            <Dialog.Panel className="fixed bottom-0 right-0 top-0 flex h-full w-full flex-col items-center gap-8 bg-slate-100 p-6 md:w-[390px]">
+              <div className="flex w-full items-center justify-between">
+                <p className="ml-1 font-heading text-[28px] font-medium">
+                  Search Products
+                </p>
 
-                <div className="mb-4 w-full">
-                  <Searchbar />
-                </div>
+                <button onClick={onClose} aria-label="Hide search field">
+                  <XMarkIcon className="h-9" />
+                </button>
               </div>
+
+              <SearchBar />
             </Dialog.Panel>
           </Transition.Child>
         </Dialog>
