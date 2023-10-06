9e0173130e334fe13e5d903cb0a789131e7879cb
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-10-06 12:21:11 -0400

    feat: improve seo (#47)

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,16 @@ import Container from '@/components/container';
 import Products from '@/components/products';
 import { getProducts } from '@/lib/shopify';
 
+export const runtime = 'edge';
+
+export const metadata = {
+  description:
+    "Looking for a new hat? Well, look no further! At BW Hats, we have some of the coolest hats on the market today with prices you won't find anywhere else!",
+  openGraph: {
+    type: 'website',
+  },
+};
+
 const HomePage = async () => {
   const products = await getProducts({});
 
