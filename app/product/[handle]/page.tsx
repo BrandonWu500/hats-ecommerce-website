ed3eb59782271d88c353fed3e3ff56bb57a725c4
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-10-06 18:10:30 -0400

    feat: improve opengraph (#49)

--- app/product/[handle]/page.tsx
+++ app/product/[handle]/page.tsx
@@ -6,6 +6,7 @@ import Container from '@/components/container';
 import ProductGallery from '@/components/product/gallery';
 import ProductInfo from '@/components/product/info';
 import ProductSlider from '@/components/product/slider';
+import { HIDDEN_PRODUCT_TAG } from '@/lib/constants';
 import { getCart, getProduct, getProductRecommendations } from '@/lib/shopify';
 import { Image } from '@/lib/shopify/types';
 
@@ -20,9 +21,32 @@ export async function generateMetadata({
 
   if (!product) return notFound();
 
+  const { url, width, height, altText: alt } = product.featuredImage || {};
+  const indexable = !product.tags.includes(HIDDEN_PRODUCT_TAG);
+
   return {
     title: product.seo.title || product.title,
     description: product.seo.description || product.description,
+    robots: {
+      index: indexable,
+      follow: indexable,
+      googleBot: {
+        index: indexable,
+        follow: indexable,
+      },
+    },
+    openGraph: url
+      ? {
+          images: [
+            {
+              url,
+              width,
+              height,
+              alt,
+            },
+          ],
+        }
+      : null,
   };
 }
 
