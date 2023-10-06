207e7e8dc3a51a4075cbdc6a43a99cc19d2dd111
BrandonWu500 <brwu17@gmail.com>
2023-10-06 14:55:42 -0400

    feat: add twitter metadata to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,20 @@ import { Raleway, Roboto } from 'next/font/google';
 
 import Footer from '@/components/footer';
 import Header from '@/components/header';
+import { ensureStartsWith } from '@/lib/utils';
 
 import './globals.css';
 
-const { SITE_NAME } = process.env;
+const { SITE_NAME, TWITTER_CREATOR, TWITTER_SITE } = process.env;
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : 'http://localhost:3000';
+const twitterCreator = TWITTER_CREATOR
+  ? ensureStartsWith(TWITTER_CREATOR, '@')
+  : undefined;
+const twitterSite = TWITTER_SITE
+  ? ensureStartsWith(TWITTER_SITE, 'https://')
+  : undefined;
 
 export const metadata = {
   metadataBase: new URL(baseUrl),
@@ -20,6 +27,14 @@ export const metadata = {
     follow: true,
     index: true,
   },
+  ...(twitterCreator &&
+    twitterSite && {
+      twitter: {
+        card: 'summary_large_image',
+        creator: twitterCreator,
+        site: twitterSite,
+      },
+    }),
 };
 
 const raleway = Raleway({ subsets: ['latin'], variable: '--heading-font' });
