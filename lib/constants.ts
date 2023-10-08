9732e63d79bb974bd8cc0933da24224aa94b9e9c
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-10-08 15:21:14 -0400

    feat: update shopify api version (#52)
    to fix footer links, which were not using the correct domain

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const SHOPIFY_GRAPHQL_API_ENDPOINT = '/api/2023-01/graphql.json';
+export const SHOPIFY_GRAPHQL_API_ENDPOINT = '/api/2023-10/graphql.json';
 export const HIDDEN_PRODUCT_TAG = 'nextjs-frontend-hidden';
 
 export const TAGS = {
