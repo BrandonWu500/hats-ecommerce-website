a1c1234577e691381e6eecfc5bc9fea8b0d49fea
BrandonWu500 <brwu17@gmail.com>
2023-10-15 10:55:20 -0400

    feat: adjust price on product page based on variant

--- components/product/info.tsx
+++ components/product/info.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 
 import AddToCart from '@/components/cart/add-to-cart';
@@ -7,14 +8,28 @@ import Price from '@/components/price';
 import VariantSelector from '@/components/product/variant-selector';
 import Prose from '@/components/prose';
 import Quantity from '@/components/quantity';
-import { Product } from '@/lib/shopify/types';
+import { Product, ProductVariant } from '@/lib/shopify/types';
 
 type Props = {
   product: Product;
   itemInCart: boolean;
 };
 const ProductInfo = ({ product, itemInCart }: Props) => {
+  const searchParams = useSearchParams();
   const [quantity, setQuantity] = useState(1);
+
+  const { variants } = product;
+
+  const defaultVariantId = variants.length === 1 ? variants[0]?.id : undefined;
+  const variant = variants.find((variant: ProductVariant) =>
+    variant.selectedOptions.every(
+      (option) => option.value === searchParams.get(option.name.toLowerCase())
+    )
+  );
+  const selectedVariantId = variant?.id || defaultVariantId;
+
+  const variantIdx =
+    variants.findIndex((variant) => variant.id === selectedVariantId) ?? 0;
 
   return (
     <div className="z-10 flex flex-col gap-6 xl:gap-5">
@@ -31,8 +46,14 @@ const ProductInfo = ({ product, itemInCart }: Props) => {
       {/* ADD TO CART SECTION */}
       <div className="flex flex-col items-center gap-8 xl:items-start">
         <Price
-          amount={product.priceRange.maxVariantPrice.amount}
-          currencyCode={product.priceRange.maxVariantPrice.currencyCode}
+          amount={
+            product.variants[variantIdx]?.price.amount ||
+            product.priceRange.minVariantPrice.amount
+          }
+          currencyCode={
+            product.variants[variantIdx]?.price.currencyCode ||
+            product.priceRange.minVariantPrice.currencyCode
+          }
           className="text-center font-body text-[32px] font-medium"
         />
 
