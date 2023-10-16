187edcdbf67fae847b2cd8d50466364904a56d55
BrandonWu500 <brwu17@gmail.com>
2023-10-16 16:33:17 -0400

    feat: update search results page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,8 +1,6 @@
-import Container from '@/components/container';
 import Products from '@/components/products';
-import SearchFilter from '@/components/search/filter';
 import { defaultSort, sorting } from '@/lib/constants';
-import { getCollections, getProducts } from '@/lib/shopify';
+import { getProducts } from '@/lib/shopify';
 
 export const runtime = 'edge';
 
@@ -15,8 +13,6 @@ type Props = {
   searchParams?: { [key: string]: string | string[] | undefined };
 };
 const SearchPage = async ({ searchParams }: Props) => {
-  const collections = await getCollections();
-
   const { sort, q: searchValue } = searchParams as { [key: string]: string };
   const { sortKey, reverse } =
     sorting.find((item) => item.slug === sort) || defaultSort;
@@ -25,20 +21,21 @@ const SearchPage = async ({ searchParams }: Props) => {
   const resultsText = products.length === 1 ? 'hat' : 'hats';
 
   return (
-    <Container className="flex-col pb-8">
-      <SearchFilter list={collections} title="Collections" />
-      <div className="w-full rounded-[10px] bg-orange-200 p-8 xl:px-48 xl:py-8">
-        <p className="flex flex-col text-center font-body text-[32px] text-slate-600 xl:text-[48px]">
-          <span className="capitalize">
-            Found {products.length} {resultsText} for
-          </span>
-          <span className="overflow-x-auto whitespace-nowrap pb-2 text-[36px] font-medium xl:text-[56px] xl:scrollbar xl:scrollbar-track-orange-100 xl:scrollbar-thumb-slate-600">
-            {searchValue}
-          </span>
-        </p>
-      </div>
+    <>
+      {searchValue && (
+        <div className="w-full rounded-[10px] bg-orange-200 p-8 xl:px-48 xl:py-8">
+          <p className="flex flex-col text-center font-body text-[32px] text-slate-600 xl:text-[48px]">
+            <span className="capitalize">
+              Found {products.length} {resultsText} for
+            </span>
+            <span className="overflow-x-auto whitespace-nowrap pb-2 text-[36px] font-medium xl:text-[56px] xl:scrollbar xl:scrollbar-track-orange-100 xl:scrollbar-thumb-slate-600">
+              {searchValue}
+            </span>
+          </p>
+        </div>
+      )}
       <Products products={products} />
-    </Container>
+    </>
   );
 };
 export default SearchPage;
