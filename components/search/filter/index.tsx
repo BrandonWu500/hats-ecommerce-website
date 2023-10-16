9d3232aa3f54737f80604323f3f15147d74be703
BrandonWu500 <brwu17@gmail.com>
2023-10-16 14:55:55 -0400

    feat: add search filter functionality

--- components/search/filter/index.tsx
+++ components/search/filter/index.tsx
@@ -2,8 +2,10 @@
 
 import { Listbox, Transition } from '@headlessui/react';
 import { ChevronUpDownIcon } from '@heroicons/react/24/solid';
-import { Fragment, useState } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { Fragment, useEffect, useState } from 'react';
 
+import { SearchFilterItem } from '@/components/search/filter/item';
 import { ListItem } from '@/lib/constants';
 
 type Props = {
@@ -11,7 +13,20 @@ type Props = {
   title?: string;
 };
 const SearchFilter = ({ list, title }: Props) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
   const [selected, setSelected] = useState(list[0]);
+
+  useEffect(() => {
+    list.forEach((listItem: ListItem) => {
+      if (
+        ('path' in listItem && pathname === listItem.path) ||
+        ('slug' in listItem && searchParams.get('sort') === listItem.slug)
+      ) {
+        setSelected(listItem);
+      }
+    });
+  }, [pathname, list, searchParams]);
 
   return (
     <div className="mb-8 w-full font-body text-2xl">
@@ -46,17 +61,7 @@ const SearchFilter = ({ list, title }: Props) => {
                   }
                   value={item}
                 >
-                  {({ selected }) => (
-                    <>
-                      <span
-                        className={`block truncate text-2xl ${
-                          selected ? 'font-medium' : 'font-normal'
-                        }`}
-                      >
-                        {item.title}
-                      </span>
-                    </>
-                  )}
+                  <SearchFilterItem item={item} />
                 </Listbox.Option>
               ))}
             </Listbox.Options>
@@ -67,62 +72,3 @@ const SearchFilter = ({ list, title }: Props) => {
   );
 };
 export default SearchFilter;
-
-// export default function Example() {
-//   const [selected, setSelected] = useState(people[0])
-
-//   return (
-//     <div className="fixed top-16 w-72">
-//       <Listbox value={selected} onChange={setSelected}>
-//         <div className="relative mt-1">
-//           <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-3 pl-4 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-//             <span className="block truncate">{selected.name}</span>
-//             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-//               <ChevronUpDownIcon
-//                 className="h-5 w-5 text-gray-400"
-//                 aria-hidden="true"
-//               />
-//             </span>
-//           </Listbox.Button>
-//           <Transition
-//             as={Fragment}
-//             leave="transition ease-in duration-100"
-//             leaveFrom="opacity-100"
-//             leaveTo="opacity-0"
-//           >
-//             <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-//               {people.map((person, personIdx) => (
-//                 <Listbox.Option
-//                   key={personIdx}
-//                   className={({ active }) =>
-//                     `relative cursor-default select-none py-3 pl-10 pr-4 ${
-//                       active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
-//                     }`
-//                   }
-//                   value={person}
-//                 >
-//                   {({ selected }) => (
-//                     <>
-//                       <span
-//                         className={`block truncate ${
-//                           selected ? 'font-medium' : 'font-normal'
-//                         }`}
-//                       >
-//                         {person.name}
-//                       </span>
-//                       {selected ? (
-//                         <span className="absolute inset-y-0 left-0 flex items-center pl-4 text-amber-600">
-//                           <CheckIcon className="h-5 w-5" aria-hidden="true" />
-//                         </span>
-//                       ) : null}
-//                     </>
-//                   )}
-//                 </Listbox.Option>
-//               ))}
-//             </Listbox.Options>
-//           </Transition>
-//         </div>
-//       </Listbox>
-//     </div>
-//   )
-// }
