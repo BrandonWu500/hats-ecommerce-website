bec838d025b5271a2ddba05508279e42e8406a83
BrandonWu500 <brwu17@gmail.com>
2023-10-17 11:53:58 -0400

    feat: reduce search page products vertical spacing

--- components/search/products.tsx
+++ components/search/products.tsx
@@ -3,17 +3,11 @@ import { Product } from '@/lib/shopify/types';
 
 type Props = {
   products: Product[];
-  title?: string;
 };
-const SearchProducts = ({ products, title }: Props) => {
+const SearchProducts = ({ products }: Props) => {
   return (
     <div className="mb-16">
-      {title && (
-        <h2 className="mb-12 text-center font-heading text-[32px] font-semibold xl:text-left xl:text-5xl">
-          {title}
-        </h2>
-      )}
-      <ul className="grid grid-cols-1 gap-16 xl:grid-cols-2 xl:gap-x-8 xl:gap-y-[72px]">
+      <ul className="grid grid-cols-1 gap-12 xl:grid-cols-2 xl:gap-x-8 xl:gap-y-[48px]">
         {products?.map((product) => (
           <ProductCard
             key={product.handle}
