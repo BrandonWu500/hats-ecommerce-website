1c4f3b4ebb10e9b5ffe4e72adda928e161dc33b3
Brandon Wu <96086441+BrandonWu500@users.noreply.github.com>
2023-10-23 15:46:43 -0400

    feat: update favicon and logo (#59)

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,11 +1,17 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import desktopLogo from '/public/desktop-logo.svg';
+import mobileLogo from '/public/mobile-logo.svg';
 
 const Logo = () => {
   return (
     <Link href={'/'} className="flex items-center gap-2 py-4  xl:gap-4">
-      <p className="font-heading text-3xl font-semibold text-orange-200">
-        BW Hats
-      </p>
+      <div className="hidden lg:block">
+        <Image src={desktopLogo} alt={`${process.env.SITE_NAME} logo`} />
+      </div>
+      <div className="block lg:hidden">
+        <Image src={mobileLogo} alt={`${process.env.SITE_NAME} logo`} />
+      </div>
     </Link>
   );
 };
