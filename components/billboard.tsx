e3565c457d20fd4c378c2be1626602814cd2f7ee
BrandonWu500 <brwu17@gmail.com>
2023-10-28 16:09:51 -0400

    feat: update mobile billboard component

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -1,9 +1,24 @@
+import { ArrowRightCircleIcon } from '@heroicons/react/24/solid';
+import Link from 'next/link';
+
 const Billboard = () => {
   return (
-    <div className="w-full rounded-[10px] bg-orange-200 px-8 py-12 xl:px-24 xl:py-16">
-      <h1 className="text-center font-heading text-[40px] font-semibold uppercase leading-tight text-slate-600 xl:text-[72px]">
-        Find the Perfect Hat for You!
+    <div className="billboard-gradient flex w-full flex-col gap-3 rounded-[10px] bg-orange-200 px-8 py-6 xl:px-24 xl:py-16">
+      <h1 className="whitespace-nowrap text-center font-heading text-2xl font-semibold  leading-tight text-slate-600 xl:text-[72px]">
+        Welcome To BW Hats!
       </h1>
+      <p className="text-center font-heading text-lg leading-tight text-slate-600">
+        Where Custom Designs Meet Handcrafted Perfection
+      </p>
+      <Link
+        href="/our-story"
+        className="mt-4 flex items-center justify-center gap-2 self-center rounded-full bg-slate-600 px-8 py-3"
+      >
+        <p className="font-heading text-lg font-semibold text-orange-200">
+          Read Our Story
+        </p>
+        <ArrowRightCircleIcon className="h-6 text-orange-200" />
+      </Link>
     </div>
   );
 };
