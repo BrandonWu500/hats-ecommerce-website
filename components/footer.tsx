afad18f70ec708eccc1dcb9234148e6c4db5e638
BrandonWu500 <brwu17@gmail.com>
2023-10-30 12:30:02 -0400

    feat: add social icons to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,11 @@
 import Link from 'next/link';
+import { SiFacebook, SiInstagram, SiTiktok, SiTwitter } from 'react-icons/si';
 
 import Container from '@/components/container';
+import Icon from '@/components/icon';
 import { getMenu } from '@/lib/shopify';
+
+const socialIcons = [SiFacebook, SiInstagram, SiTiktok, SiTwitter];
 
 const Footer = async () => {
   const menu = await getMenu('footer');
@@ -9,15 +13,20 @@ const Footer = async () => {
   return (
     <div className="bg-slate-600 text-orange-200">
       <Container className="flex-col">
-        <ul className="flex flex-col items-center justify-center gap-6 py-6 pb-10 font-body text-lg xl:flex-row xl:gap-8">
+        <ul className="flex w-full flex-col items-center justify-between gap-6 py-6 font-body text-lg xl:flex-row xl:gap-8">
           {menu.map((item) => (
             <li key={item.title}>
               <Link href={item.path}>{item.title}</Link>
             </li>
           ))}
         </ul>
-        <div className="w-full border-t border-orange-200">
-          <p className="pb-8 pt-4 text-center font-body">{`© 2023 BW Hats Inc. All rights reserved.`}</p>
+        <div className="flex w-full flex-col-reverse items-center gap-6 border-t border-orange-200 py-6 xl:flex-row">
+          <p className="flex-1 text-center xl:text-left">{`© 2023 BW Hats Inc. All rights reserved.`}</p>
+          <div className="flex w-full flex-1 items-center justify-center gap-14 px-8 xl:justify-end xl:gap-7 xl:p-0">
+            {socialIcons.map((icon, idx) => (
+              <Icon key={idx} icon={icon} className="h-6 w-6 cursor-pointer" />
+            ))}
+          </div>
         </div>
       </Container>
     </div>
